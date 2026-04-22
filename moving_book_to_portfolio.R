749fa3dc01d1ab1a78805bf2fb36442f08da8df9
BoS771401 <bo.steinschuld@outlook.com>
2026-04-22 10:34:04 +0200

    files kopieren van portfolio map naar github.io

---
+++ moving_book_to_portfolio.R
@@ -0,0 +1,5 @@
+file.copy(
+  from = list.files("/home/bo.steinschuld/dsfb2_workflows_portfolio/_book", full.names = TRUE),
+  to   = "/home/bo.steinschuld/BoS771401.github.io",
+  recursive = TRUE
+)
